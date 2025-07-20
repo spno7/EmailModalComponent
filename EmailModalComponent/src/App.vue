41a31d86733bfe8aedf92e3ac91f18d98552e32b
spno7 <spironoti@hotmail.com>
2025-07-20 20:37:04 +0300

    Create EmailModal initial component

--- EmailModalComponent/src/App.vue
+++ EmailModalComponent/src/App.vue
@@ -1,14 +1,23 @@
+<template>
+  <div id="app">  
+    
+    <h1 class="mt-3 text-info text-center">
+      Hello world
+    </h1>
+    
+    <EmailModal />
+
+  </div>
+</template>
+
+
 <script setup>
+
+import EmailModal from "./components/EmailModal.vue"
 
 </script>
 
-<template>
-  
-  <h1 class="mt-3 text-info text-center">
-    Hello world
-  </h1>
- 
-</template>
+
 
 <style scoped>
 
---
+++ EmailModalComponent/src/components/EmailModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    
+    <!-- Button to open the Modal  -->
+    <button @click="openModal" class="btn btn-info rounded-md">
+      Send Email
+    </button>
+
+    <div v-if="isModalOpen" class="modal d-block" tabindex="-1" role="dialog" >
+      <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content rounded-lg">
+          
+          <div class="modal-header">
+           
+            <h5 class="modal-title" id="emailModalLabel">Email Form</h5>
+            
+          </div>
+          <div class="modal-body">
+              <!-- TODO: Add the form here -->
+
+              Subject: <br>
+
+              Description: <br>  
+          </div>
+         
+        </div>
+      </div>
+    </div>
+
+      <div v-if="isModalOpen" class="modal-backdrop show"></div>
+  
+  </div>
+</template>
+
+<script setup>
+  
+  import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue';
+
+  const isModalOpen = ref(false);
+
+  // Opens Modal
+  const openModal = async () => {
+    isModalOpen.value = true;
+  };
+
+</script>
+
+<style scoped>
+
+
+</style>
