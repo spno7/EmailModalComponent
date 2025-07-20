e8ed97aa8df4555147a1f096c3fc658dc8bdc037
spno7 <spironoti@hotmail.com>
2025-07-20 20:53:44 +0300

    Add close functionality on EmailModal

--- EmailModalComponent/src/components/EmailModal.vue
+++ EmailModalComponent/src/components/EmailModal.vue
@@ -12,8 +12,11 @@
           
           <div class="modal-header">
            
-            <h5 class="modal-title" id="emailModalLabel">Email Form</h5>
-            
+            <h5 class="modal-title" id="emailModalLabel"> Email Form </h5>
+            <button type="button" class="btn-close" aria-label="Close" @click="closeModal"> 
+            </button>
+
+
           </div>
           <div class="modal-body">
               <!-- TODO: Add the form here -->
@@ -27,7 +30,7 @@
       </div>
     </div>
 
-      <div v-if="isModalOpen" class="modal-backdrop show"></div>
+    <div v-if="isModalOpen" class="modal-backdrop show"></div>
   
   </div>
 </template>
@@ -43,9 +46,23 @@
     isModalOpen.value = true;
   };
 
+  // Close Modal
+  const closeModal = () => {
+    isModalOpen.value = false;
+  }
+
 </script>
 
 <style scoped>
 
+  .btn-close {
+
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath d='M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 01-1.414-1.414L6.586 8 .293 1.707a1 1 0 010-1.414z'/%3e%3c/svg%3e");
+
+    background-color: red;
+
+    opacity: 0.7;
+
+  }
 
 </style>
